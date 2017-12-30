176d2c1e4fd847b03297e55525cc8e76ef7fec78
MolodetsTovarish <misha.okner@gmail.com>
2017-12-30 18:47:42 -0500

    Fixed board display of pieces

--- examples/onitama.js
+++ examples/onitama.js
@@ -3,21 +3,33 @@ var b = jsboard.board({attach:"game", size:"5x5"});
 b.cell("each").style({width:"75px", height:"75px"});
 
 // setup pieces
-var piece_red_master    = jsboard.piece({text:"RM", textIndent:"-9999px", background:"url('images/chess/king.png') no-repeat", width:"50px", height:"50px", margin:"0 auto" });
-var piece_red_pawn      = jsboard.piece({text:"RP", textIndent:"-9999px", background:"url('images/pawn_blue.png') no-repeat", width:"50px", height:"50px", margin:"0 auto" });
-var piece_blue_master   = jsboard.piece({text:"BM", textIndent:"-9999px", background:"url('images/master_blue.png') no-repeat", width:"50px", height:"50px", margin:"0 auto" });
-var piece_blue_pawn     = jsboard.piece({text:"BP", textIndent:"-9999px", background:"url('images/pawn_blue.png') no-repeat", width:"50px", height:"50px", margin:"0 auto" });
+var piece_red_master    = jsboard.piece({text:"RM", textIndent:"-9999px", background:"url('images/white.png') no-repeat", width:"50px", height:"50px", margin:"0 auto", color:"red"});
+var piece_red_pawn      = jsboard.piece({text:"RP", textIndent:"-9999px", background:"url('images/red.png') no-repeat", width:"50px", height:"50px", margin:"0 auto", color:"red" });
+var piece_blue_master   = jsboard.piece({text:"BM", textIndent:"-9999px", background:"url('images/bknight.png') no-repeat", width:"50px", height:"50px", margin:"0 auto", color:"blue" });
+var piece_blue_pawn     = jsboard.piece({text:"BP", textIndent:"-9999px", background:"url('images/black.png') no-repeat", width:"50px", height:"50px", margin:"0 auto", color:"blue" });
 
 // create pieces to place in DOM
-var redPieces = [
+var pieces = [
     piece_red_master.clone(),
     piece_red_pawn.clone(),
     piece_red_pawn.clone(),
     piece_red_pawn.clone(),
     piece_red_pawn.clone(),
+    piece_blue_master.clone(),
+    piece_blue_pawn.clone(),
+    piece_blue_pawn.clone(),
+    piece_blue_pawn.clone(),
+    piece_blue_pawn.clone()
 ];
 
 // place pieces on board
-b.cell([4,2]).place(redPieces[0]);
-b.cell([4,0]).place();
-b.cell([4,1]).place();
+b.cell([4,2]).place(pieces[0]);
+b.cell([4,0]).place(pieces[1]);
+b.cell([4,1]).place(pieces[2]);
+b.cell([4,3]).place(pieces[3]);
+b.cell([4,4]).place(pieces[4]);
+b.cell([0,2]).place(pieces[5]);
+b.cell([0,0]).place(pieces[6]);
+b.cell([0,1]).place(pieces[7]);
+b.cell([0,3]).place(pieces[8]);
+b.cell([0,4]).place(pieces[9]);
